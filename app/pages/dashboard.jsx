334a2b6f3fccac8b313f26b14a67751e2fc0ca8e
unknown <saad123mn123@gmail.com>
2023-11-22 16:49:25 +0200

    dash

--- app/pages/dashboard.jsx
+++ app/pages/dashboard.jsx
@@ -4,7 +4,7 @@ import { RentalExport } from '@/data/info';
 
 export default  function Dashboard (){
     return (
-      <div className="flexflex-col justify-between h-full">
+      <div className="flex flex-col justify-between h-full">
       <div>
       <h1>Dashboard</h1>
       <div className="flex sm:flex-row flex-col sm:items-center justify-between w-full pt-4">
@@ -13,12 +13,12 @@ export default  function Dashboard (){
       </div>
       </div>
       <div className="flex sm:flex-row flex-col justify-between pt-8">
-      <div className="">
+      <div className="w-[30%]">
         <TotalSummaryChart />
       </div>
       <RentalRows rentalData={RentalExport} />
-      <div className="w-[30%]">
-        <h1>orders</h1>
+      <div className="w-[40%]">
+        <h1 className="text-center text-xl text-[#666666]">orders</h1>
         <LineChart />
       </div>
       </div>
