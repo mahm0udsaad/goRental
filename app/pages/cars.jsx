969f4c8096101f6ffb6a244d6ef64004f5aba1c2
unknown <saad123mn123@gmail.com>
2023-11-22 18:08:37 +0200

    full

--- app/pages/cars.jsx
+++ app/pages/cars.jsx
@@ -1,71 +1,8 @@
 import { CollabsedTable, StaticTable } from "@/components/tables";
 import { Cards } from "@/components/cards";
 import { ActionBtns } from "@/components/actionBtns";
+import { VehicleDetails } from "@/data/info";
 
-const dataArray = [
-  {
-    ID: '#1',
-    AMOUNT: '$9.54',
-    STATUS: 'Active',
-    VEHICLE: 'KIA RIO',
-    PLATE_NUMBER: 'R S L 8656 ر س ل',
-    CUSTOMER: 'manhhachkt08@gmail.com',
-    DATE: 'Dec 30, 09:42 PM',
-  },
-  {
-    ID: '#2',
-    AMOUNT: '$9.54',
-    STATUS: 'Active',
-    VEHICLE: 'KIA RIO',
-    PLATE_NUMBER: 'R S L 8656 ر س ل',
-    CUSTOMER: 'manhhachkt08@gmail.com',
-    DATE: 'Dec 30, 09:42 PM',
-  },
-  {
-    ID: '#3',
-    AMOUNT: '$9.54',
-    STATUS: 'Active',
-    VEHICLE: 'KIA RIO',
-    PLATE_NUMBER: 'R S L 8656 ر س ل',
-    CUSTOMER: 'manhhachkt08@gmail.com',
-    DATE: 'Dec 30, 09:42 PM',
-  },
-  {
-    ID: '#4',
-    AMOUNT: '$9.54',
-    STATUS: 'Active',
-    VEHICLE: 'KIA RIO',
-    PLATE_NUMBER: 'R S L 8656 ر س ل',
-    CUSTOMER: 'manhhachkt08@gmail.com',
-    DATE: 'Dec 30, 09:42 PM',
-  },
-  {
-    ID: '#5',
-    AMOUNT: '$9.54',
-    STATUS: 'Active',
-    VEHICLE: 'KIA RIO',
-    PLATE_NUMBER: 'R S L 8656 ر س ل',
-    CUSTOMER: 'manhhachkt08@gmail.com',
-    DATE: 'Dec 30, 09:42 PM',
-  },
-  {
-    ID: '#6',
-    AMOUNT: '$9.54',
-    STATUS: 'Active',
-    VEHICLE: 'KIA RIO',
-    PLATE_NUMBER: 'R S L 8656 ر س ل',
-    CUSTOMER: 'manhhachkt08@gmail.com',
-    DATE: 'Dec 30, 09:42 PM',
-  }
-];
-const tableThData = [
-"ID",
-"AMOUNT",
-"VEHICLE",
-"PLATE NUMBER",
-"CUSTOMER",
-"DATE",
-]
 const cards = [
   {title:'Total Cars',number: `120` , color:'black'},
   {title:'Rented Cars',number: `70`, color:'#ff8f00'},
@@ -82,7 +19,7 @@ export default function Cars (){
       ))}
       </div>
       <ActionBtns />
-      <CollabsedTable data={dataArray} />
+      <CollabsedTable data={VehicleDetails} />
       </div>
   )
 }
